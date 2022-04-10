1266531c7eb98721cfb53d8362418784d2e6578e
Przemek Król <przemkokr@gmail.com>
2022-04-10 17:33:00 +0200

    #6 Szkielet strony głównej. inicjalizacja SPA

---
+++ AlleDrogo.WebClient/ClientApp/src/app/models/test-data-model.ts
@@ -0,0 +1,6 @@
+export interface TestData {
+  name: string;
+  description: string;
+  someDecimalValue: number;
+  someDate: string;
+}
---
+++ AlleDrogo.WebClient/ClientApp/src/app/services/auctionService.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { Auction } from "../models/auction-model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuctionService {
+  httpClient: HttpClient = null;  
+  apiEndpoint: string = 'auction';
+  apiUrl: string = null;
+
+  constructor(http: HttpClient, @Inject('API_URL') apiUrl: string) {
+    this.httpClient = http;
+    this.apiUrl = this.getEndpointUrl(apiUrl);
+  }
+
+  private getEndpointUrl = (apiUrl: string) => {
+    return apiUrl + this.apiEndpoint;
+  }
+
+  public getAuctions() {
+    return this.httpClient.get<Auction[]>(this.apiUrl + '/get-all');
+  }
+}
