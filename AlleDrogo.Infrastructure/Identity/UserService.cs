d1fc127fe948d9df9e7a76003c7876f667128859
Przemek Król <przemkokr@gmail.com>
2022-05-15 21:25:49 +0200

    db migration

--- AlleDrogo.Infrastructure/Identity/UserService.cs
+++ AlleDrogo.Infrastructure/Identity/UserService.cs
@@ -1,20 +1,16 @@
 ﻿using AlleDrogo.Domain.Entities.AppUser;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace AlleDrogo.Infrastructure.Identity
 {
     public class UserService : IUserService
     {
-
-        private readonly IHttpContextAccessor httpContextAccessor;
         private readonly UserManager<ApplicationUser> userManager;
 
-        public UserService(IHttpContextAccessor httpContextAccessor, UserManager<ApplicationUser> userManager)
+        public UserService(UserManager<ApplicationUser> userManager)
         {
-            this.httpContextAccessor = httpContextAccessor;
             this.userManager = userManager;
         }
 
--- AlleDrogo.Persistance/Migrations/20220515192458_identity#3.Designer.cs
+++ AlleDrogo.Persistance/Migrations/20220515192458_identity#3.Designer.cs
@@ -4,14 +4,16 @@ using AlleDrogo.Persistance.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AlleDrogo.Persistance.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220515192458_identity#3")]
+    partial class identity3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
