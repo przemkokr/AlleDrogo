741799ac7ffd5cf9d5b17e494c09befcf7f501eb
Marcin M <Marcin.Milewski.92@gmail.com>
2022-06-04 16:46:06 +0200

    Get active auctions by selected participant feature provided

---
+++ AlleDrogo.Application/Query/Handlers/AuctionQueriesHandlers/GetActiveAuctionsByUserQueryHandler.cs
@@ -0,0 +1,54 @@
+﻿using AlleDrogo.Application.Query.Queries.AuctionQueries;
+using AlleDrogo.Domain.Entities.Auction;
+using AlleDrogo.Infrastructure.Identity;
+using AlleDrogo.Persistance;
+using AlleDrogo.Persistance.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlleDrogo.Application.Query.Handlers.AuctionQueriesHandlers
+{
+    public class GetActiveAuctionsByUserQueryHandler : IRequestHandler<GetActiveAuctionsByUserQuery, IEnumerable<Auction>>
+    {
+        private readonly IRepository<Auction> _auctionRepository;
+        private readonly IUserService userService;
+
+        public GetActiveAuctionsByUserQueryHandler(IRepository<Auction> auctionRepository, IUserService userService)
+        {
+            this._auctionRepository = auctionRepository;
+            this.userService = userService;
+        }
+
+
+        public async Task<IEnumerable<Auction>> Handle(GetActiveAuctionsByUserQuery request, CancellationToken cancellationToken)
+        {
+            var auctions = _auctionRepository.GetAll().Where(a => a.EndDate > DateTime.Now);
+            var usersAuctions = new List<Auction>();
+            var user = await userService.GetUser(request.UserName);
+            if (user == null)
+            {
+                throw new ValidationException("User not found!");
+            }
+
+            foreach (var auction in auctions)
+            {
+                var bids = auction.Bids;
+                foreach(var bid in bids)
+                {
+                    if(bid.User.Id == user.Id)
+                    {
+                        usersAuctions.Add(auction);
+                    }
+                    break;
+                }
+            }
+            await Task.CompletedTask;
+            return usersAuctions;
+        }
+    }
+}
---
+++ AlleDrogo.Application/Query/Queries/AuctionQueries/GetActiveAuctionsByUserQuery.cs
@@ -0,0 +1,12 @@
+﻿using AlleDrogo.Domain.Entities.AppUser;
+using AlleDrogo.Domain.Entities.Auction;
+using MediatR;
+using System.Collections.Generic;
+
+namespace AlleDrogo.Application.Query.Queries.AuctionQueries
+{
+    public class GetActiveAuctionsByUserQuery : IRequest<IEnumerable<Auction>>
+    {
+        public string UserName { get; set; }
+    }
+}
